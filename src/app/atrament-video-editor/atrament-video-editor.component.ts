f9eef799abc447ff81754aad9e9113139fcdd9f8
zanul <sahilsiddiqui78622@gmail.com>
2021-02-18 15:12:07 +0530

    higlight-feature

--- src/app/atrament-video-editor/atrament-video-editor.component.ts
+++ src/app/atrament-video-editor/atrament-video-editor.component.ts
@@ -23,7 +23,11 @@ export class AtramentVideoEditorComponent implements OnInit {
   currentDrawingTool: string = "";
 
   videoStatus = {
-    playStatus: "play",
+    playStatus: "pause",
+    volume: 0.7,
+    currentTime: 0,
+    totalTime: 0,
+    isNotFullScreen: true,
     isEnded: false,
   };
 
@@ -32,6 +36,8 @@ export class AtramentVideoEditorComponent implements OnInit {
   canvasCtx;
 
   currentTime;
+
+  currentHighlighterTime: number = null;
 
   currentRenderObj;
 
@@ -47,6 +53,8 @@ export class AtramentVideoEditorComponent implements OnInit {
     endTime: 0,
     randomNum: null,
     isStartRecorded: false,
+    previousX: null,
+    previousY: null,
   };
 
   drawingDetails = {
@@ -56,23 +64,26 @@ export class AtramentVideoEditorComponent implements OnInit {
 
   timer;
 
+  highLighterTimer;
+
   constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
     this.canvasCtx = this.canvas.nativeElement.getContext("2d");
     this.madeVideoObserver();
   }
 
-  ngAfterViewInit() {}
-
   metaDataLoaded() {
-    if (this.videoStatus.playStatus === "pause" || this.videoStatus.isEnded) {
+    if (this.videoStatus.isEnded) {
       return;
     }
     const vh = this.video.nativeElement.videoHeight;
     const vw = this.video.nativeElement.videoWidth;
     this.canvas.nativeElement.width = vw;
     this.canvas.nativeElement.height = vh;
+    this.videoStatus.totalTime = this.video.nativeElement.duration;
     this.handleDraw();
   }
 
@@ -81,7 +92,39 @@ export class AtramentVideoEditorComponent implements OnInit {
     this.timer = requestAnimationFrame(() => {
       this.handleDraw();
       if (this.isPainting) {
-        this.startDrawPaint(arr);
+        if (this.currentDrawingTool == "highLight") {
+          this.startDrawHiglightPaint(arr);
+        } else {
+          this.startDrawPaint(arr);
+        }
+        if (Object.keys(this.canvasDrawing).length !== 0) {
+          for (let key in this.canvasDrawing) {
+            if (
+              Math.floor(this.video.nativeElement.currentTime) -
+                parseInt(key.split("-")[2]) >=
+                0 &&
+              Math.floor(this.video.nativeElement.currentTime) -
+                parseInt(key.split("-")[2]) <=
+                5
+            ) {
+              this.startDrawPaint(this.canvasDrawing[key]);
+            }
+          }
+        }
+        if (Object.keys(this.canvasHighlightDrawing).length !== 0) {
+          for (let key in this.canvasHighlightDrawing) {
+            if (
+              this.video.nativeElement.currentTime -
+                parseFloat(key.split("-")[2]) >=
+                0 &&
+              this.video.nativeElement.currentTime -
+                parseFloat(key.split("-")[2]) <=
+                0.1
+            ) {
+              this.startDrawHiglightPaint(this.canvasHighlightDrawing[key]);
+            }
+          }
+        }
       } else if (!this.isPainting) {
         if (Object.keys(this.canvasDrawing).length !== 0) {
           for (let key in this.canvasDrawing) {
@@ -100,110 +143,85 @@ export class AtramentVideoEditorComponent implements OnInit {
         if (Object.keys(this.canvasHighlightDrawing).length !== 0) {
           for (let key in this.canvasHighlightDrawing) {
             if (
-              Math.floor(this.video.nativeElement.currentTime) -
-                parseInt(key.split("-")[2]) >=
+              this.video.nativeElement.currentTime -
+                parseFloat(key.split("-")[2]) >=
                 0 &&
-              Math.floor(this.video.nativeElement.currentTime) -
-                parseInt(key.split("-")[2]) <=
-                1
+              this.video.nativeElement.currentTime -
+                parseFloat(key.split("-")[2]) <=
+                0.1
             ) {
-              this.startDrawPaint(this.canvasHighlightDrawing[key]);
+              this.startDrawHiglightPaint(this.canvasHighlightDrawing[key]);
             }
           }
         }
       }
     });
   };
+  count = 1;
+  handleCheckCircleOverlap() {
+    if (this.count === 1) {
+      this.count = 0;
+      return true;
+    } else {
+      this.count++;
+      return false;
+    }
+    // var DistanceX = xAxis - pxAxis;
+    // var DistanceY = yAxis - pyAxis;
+    // var DistanceCenter = Math.sqrt(
+    //   DistanceX * DistanceX + DistanceY * DistanceY
+    // );
+    // var CollisionDistance = 5;
+    // CollisionDistance += 5;
+    // if (DistanceCenter >= CollisionDistance) return true;
+    // else return false;
+  }
 
   handleStartPaint(e) {
-    const precentageAxisObj = this.convertAxisToPercent(e.offsetX, e.offsetY);
-    this.currentTime = Math.floor(this.video.nativeElement.currentTime);
-    if (this.currentDrawingTool === "annotate") {
-      if (
-        Object.keys(this.canvasDrawing).length === 0 ||
-        !this.pointInfo.isStartRecorded
-      ) {
-        this.pointInfo["isStartRecorded"] = true;
-        this.pointInfo.startTime = Math.floor(
-          this.video.nativeElement.currentTime
-        );
-        this.pointInfo.endTime = this.pointInfo.startTime;
-        this.pointInfo.randomNum = Math.random();
-        this.canvasDrawing[
-          `${this.pointInfo.startTime} - ${this.pointInfo.randomNum} - ${this.pointInfo.endTime}`
-        ] = [];
-      } else {
-        const oldEndTime = this.pointInfo.endTime;
-        if (this.currentTime > this.pointInfo.endTime) {
-          this.pointInfo.endTime = this.currentTime;
-          Object.defineProperty(
-            this.canvasDrawing,
-            `${this.pointInfo.startTime} - ${this.pointInfo.randomNum} - ${this.pointInfo.endTime}`,
-            Object.getOwnPropertyDescriptor(
+    if (
+      this.currentDrawingTool === "annotate" ||
+      this.currentDrawingTool === "highLight"
+    ) {
+      const precentageAxisObj = this.convertAxisToPercent(e.offsetX, e.offsetY);
+      this.currentTime = Math.floor(this.video.nativeElement.currentTime);
+      this.currentHighlighterTime = this.video.nativeElement.currentTime;
+
+      if (this.currentDrawingTool === "annotate") {
+        if (
+          Object.keys(this.canvasDrawing).length === 0 ||
+          !this.pointInfo.isStartRecorded
+        ) {
+          this.pointInfo["isStartRecorded"] = true;
+          this.pointInfo.startTime = Math.floor(
+            this.video.nativeElement.currentTime
+          );
+          this.pointInfo.endTime = this.pointInfo.startTime;
+          this.pointInfo.randomNum = Math.random();
+          this.canvasDrawing[
+            `${this.pointInfo.startTime} - ${this.pointInfo.randomNum} - ${this.pointInfo.endTime}`
+          ] = [];
+        } else {
+          const oldEndTime = this.pointInfo.endTime;
+          if (this.currentTime > this.pointInfo.endTime) {
+            this.pointInfo.endTime = this.currentTime;
+            Object.defineProperty(
               this.canvasDrawing,
+              `${this.pointInfo.startTime} - ${this.pointInfo.randomNum} - ${this.pointInfo.endTime}`,
+              Object.getOwnPropertyDescriptor(
+                this.canvasDrawing,
+                `${this.pointInfo.startTime} - ${this.pointInfo.randomNum} - ${oldEndTime}`
+              )
+            );
+            delete this.canvasDrawing[
               `${this.pointInfo.startTime} - ${this.pointInfo.randomNum} - ${oldEndTime}`
-            )
-          );
-          delete this.canvasDrawing[
-            `${this.pointInfo.startTime} - ${this.pointInfo.randomNum} - ${oldEndTime}`
-          ];
+            ];
+          }
         }
-      }
-    }
 
-    if (this.currentDrawingTool === "highLight") {
-      if (
-        Object.keys(this.canvasHighlightDrawing).length === 0 ||
-        !this.pointInfoHiglight.isStartRecorded
-      ) {
-        this.pointInfoHiglight["isStartRecorded"] = true;
-        this.pointInfoHiglight.startTime = Math.floor(
-          this.video.nativeElement.currentTime
-        );
-        this.pointInfoHiglight.endTime = this.pointInfoHiglight.startTime;
-        this.pointInfoHiglight.randomNum = Math.random();
-        this.canvasHighlightDrawing[
-          `${this.pointInfoHiglight.startTime} - ${this.pointInfoHiglight.randomNum} - ${this.pointInfoHiglight.endTime}`
-        ] = [];
-      } else {
-        const oldEndTime = this.pointInfoHiglight.endTime;
-        if (this.currentTime > this.pointInfoHiglight.endTime) {
-          this.pointInfoHiglight.endTime = this.currentTime;
-          Object.defineProperty(
-            this.canvasHighlightDrawing,
-            `${this.pointInfoHiglight.startTime} - ${this.pointInfoHiglight.randomNum} - ${this.pointInfoHiglight.endTime}`,
-            Object.getOwnPropertyDescriptor(
-              this.canvasHighlightDrawing,
-              `${this.pointInfoHiglight.startTime} - ${this.pointInfoHiglight.randomNum} - ${oldEndTime}`
-            )
-          );
-          delete this.canvasHighlightDrawing[
-            `${this.pointInfoHiglight.startTime} - ${this.pointInfoHiglight.randomNum} - ${oldEndTime}`
-          ];
-        }
-      }
-    }
+        this.currentRenderObj = `${this.pointInfo.startTime} - ${this.pointInfo.randomNum} - ${this.pointInfo.endTime}`;
 
-    if (this.currentDrawingTool === "annotate") {
-      this.currentRenderObj = `${this.pointInfo.startTime} - ${this.pointInfo.randomNum} - ${this.pointInfo.endTime}`;
-    }
-
-    if (this.currentDrawingTool === "highLight") {
-      this.currentRenderObj = `${this.pointInfoHiglight.startTime} - ${this.pointInfoHiglight.randomNum} - ${this.pointInfoHiglight.endTime}`;
-    }
-
-    if (this.currentDrawingTool === "annotate") {
-      if (e.type == "mousedown") {
-        this.isPainting = true;
-        if (this.canvasDrawing[`${this.currentRenderObj}`]) {
-          this.canvasDrawing[`${this.currentRenderObj}`].push(
-            precentageAxisObj
-          );
-        }
-        cancelAnimationFrame(this.timer);
-        this.handleDraw(this.canvasDrawing[`${this.currentRenderObj}`]);
-      } else {
-        if (this.isPainting) {
+        if (e.type == "mousedown") {
+          this.isPainting = true;
           if (this.canvasDrawing[`${this.currentRenderObj}`]) {
             this.canvasDrawing[`${this.currentRenderObj}`].push(
               precentageAxisObj
@@ -211,34 +229,73 @@ export class AtramentVideoEditorComponent implements OnInit {
           }
           cancelAnimationFrame(this.timer);
           this.handleDraw(this.canvasDrawing[`${this.currentRenderObj}`]);
+        } else {
+          if (this.isPainting) {
+            if (this.canvasDrawing[`${this.currentRenderObj}`]) {
+              this.canvasDrawing[`${this.currentRenderObj}`].push(
+                precentageAxisObj
+              );
+            }
+            cancelAnimationFrame(this.timer);
+            this.handleDraw(this.canvasDrawing[`${this.currentRenderObj}`]);
+          }
         }
       }
-    }
 
-    if (this.currentDrawingTool === "highLight") {
-      if (e.type == "mousedown") {
-        this.isPainting = true;
-        if (this.canvasHighlightDrawing[`${this.currentRenderObj}`]) {
-          this.canvasHighlightDrawing[`${this.currentRenderObj}`].push(
-            precentageAxisObj
-          );
+      if (this.currentDrawingTool === "highLight") {
+        if (e.type === "mousedown") {
+          this.isPainting = true;
         }
-        cancelAnimationFrame(this.timer);
-        this.handleDraw(
-          this.canvasHighlightDrawing[`${this.currentRenderObj}`]
-        );
-      } else {
-        if (this.isPainting) {
-          if (this.canvasHighlightDrawing[`${this.currentRenderObj}`]) {
-            this.canvasHighlightDrawing[`${this.currentRenderObj}`].push(
-              precentageAxisObj
+
+        if (
+          (Object.keys(this.canvasHighlightDrawing).length === 0 ||
+            !this.pointInfoHiglight.isStartRecorded) &&
+          this.isPainting
+        ) {
+          this.pointInfoHiglight["isStartRecorded"] = true;
+          this.pointInfoHiglight.startTime = this.video.nativeElement.currentTime;
+          this.pointInfoHiglight.endTime = this.video.nativeElement.currentTime;
+          this.pointInfoHiglight.randomNum = Math.random();
+          this.canvasHighlightDrawing[
+            `${this.pointInfoHiglight.startTime} - ${this.pointInfoHiglight.randomNum} - ${this.pointInfoHiglight.endTime}`
+          ] = {};
+        }
+
+        this.currentRenderObj = `${this.pointInfoHiglight.startTime} - ${this.pointInfoHiglight.randomNum} - ${this.pointInfoHiglight.endTime}`;
+
+        if (e.type == "mousedown") {
+          if (this.isPainting && this.handleCheckCircleOverlap()) {
+            if (this.canvasHighlightDrawing[`${this.currentRenderObj}`]) {
+              this.canvasHighlightDrawing[`${this.currentRenderObj}`] = {
+                ...precentageAxisObj,
+              };
+            }
+            cancelAnimationFrame(this.timer);
+            this.handleDraw(
+              this.canvasHighlightDrawing[`${this.currentRenderObj}`]
             );
+            this.pointInfoHiglight.previousX = e.offsetX;
+            this.pointInfoHiglight.previousY = e.offsetY;
           }
-          cancelAnimationFrame(this.timer);
-          this.handleDraw(
-            this.canvasHighlightDrawing[`${this.currentRenderObj}`]
-          );
+        } else {
+          if (this.isPainting && this.handleCheckCircleOverlap()) {
+            if (this.canvasHighlightDrawing[`${this.currentRenderObj}`]) {
+              this.canvasHighlightDrawing[`${this.currentRenderObj}`] = {
+                ...precentageAxisObj,
+              };
+            }
+            cancelAnimationFrame(this.timer);
+            this.handleDraw(
+              this.canvasHighlightDrawing[`${this.currentRenderObj}`]
+            );
+            this.pointInfoHiglight.previousX = e.offsetX;
+            this.pointInfoHiglight.previousY = e.offsetY;
+          }
         }
+        this.pointInfoHiglight.endTime = null;
+        this.pointInfoHiglight.startTime = null;
+        this.pointInfoHiglight.randomNum = null;
+        this.pointInfoHiglight.isStartRecorded = false;
       }
     }
   }
@@ -260,7 +317,6 @@ export class AtramentVideoEditorComponent implements OnInit {
   handleStopPaint() {
     this.isPainting = false;
     this.canvasCtx.beginPath();
-
     if (this.currentDrawingTool === "annotate") {
       this.canvasDrawing[this.currentRenderObj] = this.canvasDrawing[
         this.currentRenderObj
@@ -271,15 +327,16 @@ export class AtramentVideoEditorComponent implements OnInit {
       this.pointInfo.isStartRecorded = false;
     }
     if (this.currentDrawingTool === "highLight") {
-      this.canvasHighlightDrawing[
-        this.currentRenderObj
-      ] = this.canvasHighlightDrawing[this.currentRenderObj].filter(
-        (ele, i) => i % 2 === 0
-      );
       this.pointInfoHiglight.endTime = null;
       this.pointInfoHiglight.startTime = null;
       this.pointInfoHiglight.randomNum = null;
       this.pointInfoHiglight.isStartRecorded = false;
+      for (let key in this.canvasHighlightDrawing) {
+        if (Object.keys(this.canvasHighlightDrawing[key]).length === 0) {
+          delete this.canvasHighlightDrawing[key];
+        }
+      }
+      console.log(this.canvasHighlightDrawing);
     }
   }
 
@@ -320,6 +377,21 @@ export class AtramentVideoEditorComponent implements OnInit {
     }
   };
 
+  startDrawHiglightPaint = (obj) => {
+    if (obj != undefined) {
+      const { xAxis, yAxis } = this.convertPercentageToAxis(
+        obj["x-axis"],
+        obj["y-axis"]
+      );
+      this.canvasCtx.beginPath();
+      this.canvasCtx.arc(xAxis, yAxis, 5, 0, 2 * Math.PI);
+      this.canvasCtx.fillStyle = obj["stroke-color"];
+      this.canvasCtx.strokeStyle = obj["stroke-color"];
+      this.canvasCtx.fill();
+      this.canvasCtx.stroke();
+    }
+  };
+
   madeVideoObserver() {
     this.videoObservable = new Observable((observer) => {
       setInterval(() => {
@@ -331,16 +403,34 @@ export class AtramentVideoEditorComponent implements OnInit {
   subscribeVideoObserver() {
     this.videoObserver = this.videoObservable.subscribe((data) => {
       this.currentVideoDuration = data;
+      this.videoStatus.currentTime = this.currentVideoDuration;
     });
   }
 
-  handleVideoClick(e) {
-    if (e.type === "play") {
+  handleVideoClick({ type }) {
+    if (type === "play") {
       this.subscribeVideoObserver();
       this.videoStatus.playStatus = "play";
-    } else if (e.type === "pause") {
+      this.video.nativeElement.play();
+    } else if (type === "pause") {
       this.videoObserver.unsubscribe();
       this.videoStatus.playStatus = "pause";
+      this.video.nativeElement.pause();
+    } else if (type === "volume") {
+      this.video.nativeElement.volume = this.videoStatus.volume;
+    } else if (type === "slider") {
+      this.video.nativeElement.currentTime = this.videoStatus.currentTime;
+      if (this.videoStatus.playStatus === "play") {
+        this.videoStatus.playStatus = "play";
+        this.video.nativeElement.play();
+      } else if (this.videoStatus.playStatus === "pause") {
+        this.videoStatus.playStatus = "pause";
+        this.video.nativeElement.pause();
+      }
+    } else if (type === "fullScreen") {
+      this.videoStatus.isNotFullScreen = !this.videoStatus.isNotFullScreen;
+      // this.canvas.nativeElement.width = window.innerWidth;
+      // this.canvas.nativeElement.height = window.innerHeight;
     }
   }
 
